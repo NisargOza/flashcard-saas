7dd5b4ba1f3a4e7554a64345b4d852958061369f
NisargOza <oza.nisarg2009@gmail.com>
2024-08-12 21:44:38 -0400

    firebase keys

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAo05gScapJabST0JfBCs_hOGWOwf8eXE8",
-  authDomain: "flashcardsaas-1b7c5.firebaseapp.com",
-  projectId: "flashcardsaas-1b7c5",
-  storageBucket: "flashcardsaas-1b7c5.appspot.com",
-  messagingSenderId: "372188403913",
-  appId: "1:372188403913:web:4939f35a256996cf0bd518",
-  measurementId: "G-HDX93F3LWJ"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
