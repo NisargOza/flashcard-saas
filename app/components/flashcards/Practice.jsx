619ae507f2f15446c17daa7d80d2a1299d93f6a1
raglandconnor <raglandconnor@ufl.edu>
2024-08-16 11:07:50 -0400

    Add tooltips

--- app/components/flashcards/Practice.jsx
+++ app/components/flashcards/Practice.jsx
@@ -1,19 +1,25 @@
-"use client";
-import React from "react";
-import { Container } from "../ui/craft";
-import { titleCase } from "@/app/lib/helpers";
-import { FlashcardVerticalRotation } from "./Flashcard";
-import EmblaCarousel from "../carousel/EmblaCarousel";
-import FlashcardsGrid from "./FlashcardsGrid";
-import { Button } from "../ui/button";
-import { Pencil, Trash } from "lucide-react";
-import Link from "next/link";
-import { VIEW_FLASHCARD_SETS_URL } from "@/app/lib/constants";
-import { deleteFlashcardSet } from "@/app/lib/firebase";
-import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+'use client';
+import React from 'react';
+import { Container } from '../ui/craft';
+import { titleCase } from '@/app/lib/helpers';
+import { FlashcardVerticalRotation } from './Flashcard';
+import EmblaCarousel from '../carousel/EmblaCarousel';
+import FlashcardsGrid from './FlashcardsGrid';
+import { Button } from '../ui/button';
+import { Pencil, Trash } from 'lucide-react';
+import Link from 'next/link';
+import { VIEW_FLASHCARD_SETS_URL } from '@/app/lib/constants';
+import { deleteFlashcardSet } from '@/app/lib/firebase';
+import { useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../ui/tooltip';
 
-const OPTIONS = { axis: "y" };
+const OPTIONS = { axis: 'y' };
 
 export default function Practice({ title, flashcards }) {
   const [isDeleting, setIsDeleting] = React.useState(false);
@@ -39,20 +45,39 @@ export default function Practice({ title, flashcards }) {
       <div className="flex flex-row items-center justify-between">
         <h1 className="my-8 text-3xl font-bold">Terms ({flashcards.length})</h1>
         <div className="flex flex-row gap-2">
-          <Link
-            className="flex items-center rounded-md bg-gray-700 p-2 align-middle text-white hover:bg-gray-800"
-            href={`${VIEW_FLASHCARD_SETS_URL}/edit/${title}`}
-          >
-            <Pencil />
-          </Link>
-          <Button
-            disabled={isDeleting}
-            variant="destructive"
-            onClick={handleDelete}
-            className="flex items-center rounded-md bg-red-500 p-2 align-middle text-white hover:bg-red-600"
-          >
-            <Trash />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  className="flex items-center rounded-md bg-gray-700 p-2 align-middle text-white hover:bg-gray-800"
+                  href={`${VIEW_FLASHCARD_SETS_URL}/edit/${title}`}
+                >
+                  <Pencil />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Edit set</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  disabled={isDeleting}
+                  variant="destructive"
+                  onClick={handleDelete}
+                  className="flex items-center rounded-md bg-red-500 p-2 align-middle text-white hover:bg-red-600"
+                >
+                  <Trash />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Delete Set</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
       <FlashcardsGrid flashcards={flashcards} />
